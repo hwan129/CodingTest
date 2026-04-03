04e75cb285d3c209d6445034dbb8b50c4d97408b
Seonghwan Sim <130568725+hwan129@users.noreply.github.com>
2026-04-03 12:36:29 +0900

    [level 0] Title: 카운트 다운, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.py
@@ -1,5 +1,2 @@
 def solution(start_num, end_num):
-    answer = []
-    for i in range(start_num - end_num + 1):
-        answer.append(start_num - i)
-    return answer
+    return list(range(start_num, end_num - 1, -1))
