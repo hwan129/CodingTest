761a26580e7c1c1fb649d256c4a61f53737faa11
Seonghwan Sim <130568725+hwan129@users.noreply.github.com>
2026-04-03 11:05:30 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,8 +1,9 @@
 def solution(todo_list, finished):
     answer = []
     
-    for i, k in enumerate(finished):
-        if not k:
-            answer.append(todo_list[i])
+    # for i, k in enumerate(finished):
+    #     if not k:
+    #         answer.append(todo_list[i])
     
-    return answer
+    
+    return [todo_list[i] for i, k in enumerate(finished) if not k]
