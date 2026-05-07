ea8a081d604c88ee7f28103c8fa1d12cffe85e69
Seonghwan Sim <130568725+hwan129@users.noreply.github.com>
2026-05-07 22:13:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 8.88 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,13 +1,2 @@
 def solution(my_string, s, e):
-    answer = ''
-    reverse_str = my_string[s:e+1][::-1]
-    
-    for i in range(len(my_string)):
-        if i > s and i < e + 1:
-            continue
-        elif i == s:
-            answer += reverse_str
-        else:
-            answer += my_string[i]
-        
-    return answer
+    return my_string[:s] + my_string[s:e+1][::-1] + my_string[e+1:]
