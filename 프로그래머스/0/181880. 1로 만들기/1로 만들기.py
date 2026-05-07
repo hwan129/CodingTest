c639af843d0fa1c95f994401a8dd15f2c865353f
Seonghwan Sim <130568725+hwan129@users.noreply.github.com>
2026-05-07 21:47:43 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,17 @@
+def solution(num_list):
+    answer = 0
+    
+    for i in num_list:
+        count = 0
+        while 1:
+            if i == 1:
+                break
+            if i % 2:
+                i -= 1
+                continue
+                
+            i /= 2
+            count += 1
+            
+        answer += count
+    return answer
