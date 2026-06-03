f46e87b551c9641dfea14a221bdf46532d87f47f
Seonghwan Sim <130568725+hwan129@users.noreply.github.com>
2026-06-03 17:42:58 +0900

    [level 0] Title: 세 개의 구분자, Time: 6.29 ms, Memory: 20.3 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,20 +1,21 @@
 def solution(myStr):
     answer = []
-    str = ""
+#     str = ""
     
-    for i in myStr:
-        if 'a' in i or 'b' in i or 'c' in i:
-            answer.append(str)
-            str = ""
-            continue
-        str += i
-    answer.append(str)
+#     for i in myStr:
+#         if 'a' in i or 'b' in i or 'c' in i:
+#             answer.append(str)
+#             str = ""
+#             continue
+#         str += i
+#     answer.append(str)
     
-    while "" in answer:
-        answer.remove("")
+#     while "" in answer:
+#         answer.remove("")
         
-    if answer == []:
-        return ["EMPTY"]
+#     if answer == []:
+#         return ["EMPTY"]
+
+    answer= myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split()
         
-        
-    return answer
+    return answer if answer else ["EMPTY"]
