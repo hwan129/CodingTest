8f2823cfa6627a437291ad4ac5df9cade52315f7
Seonghwan Sim <130568725+hwan129@users.noreply.github.com>
2026-06-05 13:24:53 +0900

    [level 0] Title: 2의 영역, Time: 3.93 ms, Memory: 13.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    a = []
+    
+    if 2 not in arr:
+        return [-1]
+    
+    for i,k in enumerate(arr):
+        if arr[i] == 2:
+            a.append(i)
+        
+    return arr[min(a):max(a)+1]
